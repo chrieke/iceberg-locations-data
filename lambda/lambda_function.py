e92bc9255186c941ea1daef6102e3dae8605c30c
Christoph Rieke <christoph.rieke@up42.com>
2022-12-11 22:52:12 +0100

    simplify lambda

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -9,12 +9,17 @@ from botocore.exceptions import NoCredentialsError
 
 def lambda_handler(event=None, context=None):
 
-    def query_usi(start_date: str, end_date: str, freq_days: str):
+    def is_aws_env():
+        return os.environ.get('AWS_LAMBDA_FUNCTION_NAME')
+
+    def upload_to_s3(local_file, s3_file):
+        s3 = boto3.client('s3')
+        s3.upload_file(local_file, os.environ['BUCKET_NAME'], s3_file, ExtraArgs={'ACL':'public-read'})
+
+    def query_icebergs_usi(start_date: str, end_date: str, freq_days: str):
         """
         Query US national ice center - Antarctic Iceberg Data - Archive
-
         https://usicecenter.gov/Products/AntarcIcebergs
-
         """
         usi_url = "https://usicecenter.gov/File/DownloadProduct?products=%2Ficeberg%2F{}&fName=AntarcticIcebergs_{}.csv"
 
@@ -40,46 +45,12 @@ def lambda_handler(event=None, context=None):
 
     def format_usi(df):
         df = df.drop(["Remarks", "Last Update"], axis=1)
-        # df = df.rename({"Longitude": "lon", "Latitude": "lat"}, axis=1)
         df.columns = [x.lower() for x in df.columns]
         df = df.dropna(subset=['iceberg', 'longitude', 'latitude', 'date'])
 
         # Drops problematic icebergs marked with a star
         df = df[~df["iceberg"].str.contains("*", regex=False, na=False)]
-
-        # gdf = gpd.GeoDataFrame(
-        #     df, geometry=gpd.points_from_xy(df["lon"], df["lat"], crs="EPSG:4326")
-        # )
         return df
-
-    def upload_to_aws(local_file, s3_file):
-        s3 = boto3.client('s3')
-
-        try:
-            s3.upload_file(local_file, os.environ['BUCKET_NAME'], s3_file, ExtraArgs={'ACL':'public-read'}) #
-            # requires acl for bucket enabled.
-            # and ima role required s3 > s3:PutObject, GetObjectAcl, PutObjectAcl
-
-            # url = s3.generate_presigned_url(
-            #     ClientMethod='get_object',
-            #     Params={
-            #         'Bucket': os.environ['BUCKET_NAME'],
-            #         'Key': s3_file
-            #     },
-            #     ExpiresIn=24 * 3600
-            # )
-            url = None
-            print("Upload Successful", url)
-            return url
-        except FileNotFoundError:
-            print("The file was not found")
-            return None
-        except NoCredentialsError:
-            print("Credentials not available")
-            return None
-
-    def is_aws_env():
-        return os.environ.get('AWS_LAMBDA_FUNCTION_NAME')
 
     # def most_recent_date_on_s3():
     #     s3_file = "https://usi-icebergs.s3.eu-central-1.amazonaws.com/icebergs_locations_usi.csv"
@@ -87,23 +58,21 @@ def lambda_handler(event=None, context=None):
 
     start_date = "2014-10-01"
     end_date = "2022-12-10"
-    fname = f"icebergs_locations_usi.csv"#f"usi_{start_date}_{end_date}.csv"
+    fname = f"icebergs_locations_usi.csv"
 
-    df = query_usi(start_date, end_date, "1d")
+    df = query_icebergs_usi(start_date, end_date, "1d")
     df = format_usi(df)
 
     if is_aws_env():
         df.to_csv("/tmp/" + fname)
-        url = upload_to_aws(local_file="/tmp/" + fname, s3_file=fname)
+        upload_to_s3(local_file="/tmp/" + fname, s3_file=fname)
     else:
-        url = "local"
         df.to_csv(fname)
-
     print("shape df", df.shape[0])
 
     return {
         'statusCode': 200,
-        'body': {"url": url, "df_len": df.shape[0]}
+        'body': {"df_len": df.shape[0]}
     }
 
 
